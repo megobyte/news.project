30346413a9ac521c762b6cd02605916a148b5f38
mbyte <mbyte-zeus@ya.ru>
2019-09-12 18:46:41 +0300

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+  .layout
+    .menu
+
+    nuxt
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  section.index
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
